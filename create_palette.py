f039d330a08b1f2d64abbd6fcf479ddb9043c88a
Pierandrea Guarnieri <pierandrea92@gmail.com>
2019-11-19 11:36:47 +0000

    Add files via upload
    Now can create full display including original image and palette. One example added

--- create_palette.py
+++ create_palette.py
@@ -8,10 +8,12 @@ from sklearn.cluster import KMeans
 #%%
 from PIL import Image
 
-img = Image.open('madmax.jpg')
+img = Image.open('wes.jpg')
+
+img_orig = img.copy()
 
 #%% resize for speed
-basewidth = 100
+basewidth = 800
 wpercent = (basewidth / float(img.size[0]))
 hsize = int((float(img.size[1]) * float(wpercent)))
 img = img.resize((basewidth, hsize), Image.ANTIALIAS)
@@ -26,7 +28,9 @@ b = arr[:,:,2].flatten()
 df = pd.DataFrame({'r' : list(r), 'g' : list(g), 'b' : list(b)})
 
 #%%
-kmeans = KMeans(n_clusters=10).fit(df)
+n_clusters = 10
+
+kmeans = KMeans(n_clusters=n_clusters).fit(df)
 
 centroids = kmeans.cluster_centers_
 
@@ -54,4 +58,58 @@ colours = [list(colorsys.hsv_to_rgb(*c)) for c in colours]
 plt.imshow([np.array(colours)])
 
 # %%
+from PIL import Image, ImageFont, ImageDraw, ImageEnhance
 
+full_w = 1000
+full_h = 1000
+
+new_img = Image.new('RGBA', (full_w, full_h), "white")
+
+# %%
+frame_s = round(full_w / 60)
+
+intrabox_w = round(full_w / 100)
+
+box_w = round((full_w - ((frame_s*2) + (intrabox_w*(n_clusters-1)))) / n_clusters)
+box_h = round(full_h / 4)
+
+x = frame_s
+for col in colours:
+
+    col_box = Image.new('RGBA', (box_w, box_h), tuple(np.round(np.array(col)*255).astype(int)))
+
+    new_img.paste(col_box, (x, full_h-frame_s-box_h))
+
+    x = x + box_w + intrabox_w
+
+#%%
+basewidth = round(full_w - (frame_s*2))
+baseheight = round(full_h - (frame_s + box_h) - (full_h/10*2))
+
+# adjust height to fit box
+wpercent = (baseheight / float(img_orig.size[1]))
+wsize = int((float(img_orig.size[0]) * float(wpercent)))
+img_orig = img_orig.resize((wsize, baseheight), Image.ANTIALIAS)
+
+# if width still not fitting, also adjust that
+if img_orig.size[0] > basewidth:
+    wpercent = (basewidth / float(img_orig.size[0]))
+    hsize = int((float(img_orig.size[1]) * float(wpercent)))
+    img_orig = img_orig.resize((basewidth, hsize), Image.ANTIALIAS)
+
+#%% 
+wsize = img_orig.size[0]
+hsize = img_orig.size[1]
+
+pos_x = frame_s + round((basewidth-wsize)/2)
+pos_y = round(full_h/10) + round((baseheight-hsize)/2)
+
+coords = (pos_x, pos_y)
+
+new_img.paste(img_orig, coords)
+
+# %%
+#plt.imshow(new_img)
+new_img.save("output.png", "PNG")
+
+# %%
