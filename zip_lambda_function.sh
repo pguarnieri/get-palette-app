b49e258193a55e19e499dc308cfc226eba417a5e
Pierandrea Guarnieri <p.guarnieri@me.com>
2020-06-18 21:22:25 +0100

    Created script to automatically zip packages required with lambda function and upload it to the aws function

---
+++ zip_lambda_function.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -x
+
+pip3 install -t ./packages requirements.txt
+
+cd ./packages
+zip -r9 ${OLDPWD}/function.zip .
+
+cd $OLDPWD
+zip function.zip lambda_function.py
+
+aws lambda update-function-code --function-name my-function --zip-file fileb://function.zip
