813f51f093d5f7098d2bcaae65e598ff084f5842
Pierandrea Guarnieri <p.guarnieri@me.com>
2020-06-24 13:36:14 +0100

    Improved deletion of unnecessary parts of dependencies to keep totale package size below 250MB, i.e. Lambda function limit

--- zip_lambda_function.sh
+++ zip_lambda_function.sh
@@ -3,7 +3,7 @@
 cp -r get_palette ./packages/
 
 pip3 install --target ./packages -r requirements/prod.txt
-rm -rf ./packages/**/tests
+find ./packages -iname tests -type d -exec rm -rf {} +
 
 cd ./packages
 zip -r9 ${OLDPWD}/function.zip .
