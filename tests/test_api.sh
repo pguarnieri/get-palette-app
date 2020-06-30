8ab48e44f59c0238f878dc8245e7f50f14ee7028
Pierandrea Guarnieri <p.guarnieri@me.com>
2020-06-30 22:15:58 +0100

    Updated API url

--- tests/test_api.sh
+++ tests/test_api.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -x
 
 curl -v -X POST \
-  'https://avx8addboa.execute-api.eu-west-2.amazonaws.com/beta/get-palette' \
+  'https://nqj2xhn207.execute-api.eu-west-2.amazonaws.com/beta/get-palette' \
   -H 'content-type: application/json' \
-  -d ./tests/request.json \
+  -d @./tests/request.json \
   -o ./tests/output.json
