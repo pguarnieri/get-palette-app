ad56bb28883c5709c714b8cd9ed33aa79d83b4b1
Pierandrea Guarnieri <p.guarnieri@me.com>
2020-08-23 17:14:06 +0100

    Added coarse option to accept all origins

--- lambda_function.py
+++ lambda_function.py
@@ -16,6 +16,7 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'isBase64Encoded': True,
         'headers': {
+            'Access-Control-Allow-Origin': '*',
             'content-type': 'image/png'
         },
         'body': json.dumps(img_b64)
