f25f2f5e084f29f60e1f8d5b7cf1f7eccaf44927
Pierandrea Guarnieri <p.guarnieri@me.com>
2020-09-03 21:12:21 +0100

    Now uses exif data to use correct orientation

--- get_palette/get_data.py
+++ get_palette/get_data.py
@@ -4,6 +4,25 @@ import PIL.Image
 
 def get_image_b64(img_b64):
 	
-	img = PIL.Image.open(BytesIO(b64decode(img_b64)))
+	image = PIL.Image.open(BytesIO(b64decode(img_b64)))
+
+	# rotate image according to exif
+	try:
+	    for orientation in ExifTags.TAGS.keys():
+	        if ExifTags.TAGS[orientation]=='Orientation':
+	            break
+
+	    exif=dict(image._getexif().items())
+
+	    if exif[orientation] == 3:
+	        image=image.rotate(180, expand=True)
+	    elif exif[orientation] == 6:
+	        image=image.rotate(270, expand=True)
+	    elif exif[orientation] == 8:
+	        image=image.rotate(90, expand=True)
+
+	except (AttributeError, KeyError, IndexError):
+	    # cases: image don't have getexif
+	    pass
 	
-	return img
+	return image
