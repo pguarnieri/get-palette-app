2003d454ee84f89b39d9f466f3611450c2655dc3
Pierandrea Guarnieri <p.guarnieri@me.com>
2020-12-30 16:32:10 +0000

    Fixed indentation

--- lambda_function.py
+++ lambda_function.py
@@ -5,19 +5,16 @@ from get_palette import get_data, get_colours, create_output
 def lambda_handler(event, context):
 
 	statusCode = 400
+
 	try:
+		img = get_data.get_image_b64(img_b64=event['body'])
+		c = get_colours.kmeans(img=img)
+		img_out = create_output.create_palette_image(img=img, colours=c)
+		img_b64 = create_output.image2b64(img=img_out)
 
-	    img = get_data.get_image_b64(img_b64=event['body'])
+		statusCode = 200
 
-	    c = get_colours.kmeans(img=img)
-
-	    img_out = create_output.create_palette_image(img=img, colours=c)
-
-	    img_b64 = create_output.image2b64(img=img_out)
-
-	    statusCode = 200
-
-    return {
+	return {
         'statusCode': statusCode,
         'isBase64Encoded': True,
         'headers': {
