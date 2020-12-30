89b9c7950d4fd46646c3d2c6e352cfbd752f3042
Pierandrea Guarnieri <p.guarnieri@me.com>
2020-12-30 16:25:50 +0000

    Added option to make status code 400 if code does not run fine

--- lambda_function.py
+++ lambda_function.py
@@ -4,16 +4,21 @@ from get_palette import get_data, get_colours, create_output
 
 def lambda_handler(event, context):
 
-    img = get_data.get_image_b64(img_b64=event['body'])
+	statusCode = 400
+	try:
 
-    c = get_colours.kmeans(img=img)
+	    img = get_data.get_image_b64(img_b64=event['body'])
 
-    img_out = create_output.create_palette_image(img=img, colours=c)
+	    c = get_colours.kmeans(img=img)
 
-    img_b64 = create_output.image2b64(img=img_out)
+	    img_out = create_output.create_palette_image(img=img, colours=c)
+
+	    img_b64 = create_output.image2b64(img=img_out)
+
+	    statusCode = 200
 
     return {
-        'statusCode': 200,
+        'statusCode': statusCode,
         'isBase64Encoded': True,
         'headers': {
             'content-type': 'image/png'
